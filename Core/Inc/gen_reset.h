a1ac6b3be9d1c8bff97c54b9e3f1f127d71b8e74
gokhan.tekinok <gokhan.tekinok@GEN0070.genemek.local>
2022-01-24 11:25:36 +0300

    Added reset files.

---
+++ Core/Inc/gen_reset.h
@@ -0,0 +1,8 @@
+/*
+ * gen_reset.h
+ *
+ *  Created on: Jan 24, 2022
+ *      Author: gokhan.tekinok
+ */
+
+
---
+++ Core/Src/gen_reset.c
@@ -0,0 +1,8 @@
+/*
+ * gen_reset.c
+ *
+ *  Created on: Jan 24, 2022
+ *      Author: gokhan.tekinok
+ */
+
+
